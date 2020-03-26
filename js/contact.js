d7e605c42ce4b76082a49e94df3a30f7ff98b364
Arnd <git@codefanatic.de>
2020-03-26 11:08:33 +0100

    Format html and js documents

--- js/contact.js
+++ js/contact.js
@@ -1,17 +1,13 @@
-$(function ()
-{
-    $("#contact-form").on("submit", function (e)
-    {
-        if (!e.isDefaultPrevented())
-        {
+$(function () {
+    $("#contact-form").on("submit", function (e) {
+        if (!e.isDefaultPrevented()) {
             $.ajax({
                 type: "POST",
                 url: "mail/contact.php",
                 data: $(this).serialize(),
-                success: function (data)
-                {
+                success: function (data) {
                     var messagebox = '<div class="alert ' + data.type + ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button><strong>' + data.message + '</strong></div>';
-                    
+
                     $("#contact-form").find(".form-success").html(messagebox);
                     $("#contact-form")[0].reset();
                 }
--- js/main.js
+++ js/main.js
@@ -1,61 +1,59 @@
-$(window).on('load', function(){
+$(window).on('load', function () {
     $('#load-animation').fadeOut(750);
 });
 
-$(window).scroll(function(){
+$(window).scroll(function () {
     $(".navbar").css("opacity", 1 - $(window).scrollTop() / 160);
-  });
+});
 
-$(document).ready(function(){
+$(document).ready(function () {
     "use strict";
 
     // Configuration of the pie chart
-    $('#skills').waypoint(function(){
-        $('.chart').easyPieChart({		
-            barColor:'#366cfe',
+    $('#skills').waypoint(function () {
+        $('.chart').easyPieChart({
+            barColor: '#366cfe',
             trackColor: '#ced4da',
             scaleColor: false,
-            lineWidth : 6,
+            lineWidth: 6,
             animate: 1000,
-            onStep: function(from, to, percent) {
+            onStep: function (from, to, percent) {
                 $(this.el).find('span').text(Math.round(percent));
             }
         });
-    }, 
-    {
-        triggerOnce: true, offset: '50%'
-    });
-    
+    },
+        {
+            triggerOnce: true, offset: '50%'
+        });
+
     // Animate social buttons on mouse hover event
-    $('.social-buttons').children().each(function(){
+    $('.social-buttons').children().each(function () {
         $(this).hover(
-            function() {
-                $(this).find('a').css({'-webkit-transition': 'all 0.3s', 'transition': 'all 0.3s', transform: 'translateY(-5px)'});
-                $(this).find('p').css({'-webkit-transition': 'all 0.3s', 'transition': 'all 0.3s', color: '#366cfe'});                
+            function () {
+                $(this).find('a').css({ '-webkit-transition': 'all 0.3s', 'transition': 'all 0.3s', transform: 'translateY(-5px)' });
+                $(this).find('p').css({ '-webkit-transition': 'all 0.3s', 'transition': 'all 0.3s', color: '#366cfe' });
             },
-            function() {
-                $(this).find('a').css({'-webkit-transition': 'all 0.3s', 'transition': 'all 0.3s', transform: 'none'});
-                $(this).find('p').css({'-webkit-transition': 'all 0.3s', 'transition': 'all 0.3s', color: '#6a6a6a'});                
+            function () {
+                $(this).find('a').css({ '-webkit-transition': 'all 0.3s', 'transition': 'all 0.3s', transform: 'none' });
+                $(this).find('p').css({ '-webkit-transition': 'all 0.3s', 'transition': 'all 0.3s', color: '#6a6a6a' });
             }
         );
     });
-    
+
     // Smooth scrolling using jQuery easing
-    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname)
-    {
-        var target = $(this.hash);
-        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-        if (target.length)
-        {
-            $('html, body').animate({scrollTop: (target.offset().top - 54)}, 1000, "easeInOutExpo");
-            return false;
+    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html, body').animate({ scrollTop: (target.offset().top - 54) }, 1000, "easeInOutExpo");
+                return false;
+            }
         }
-    }
     });
 
     // Closes responsive menu when a scroll trigger link is clicked
-    $('.js-scroll-trigger').click(function(){
+    $('.js-scroll-trigger').click(function () {
         $('.navbar-collapse').collapse('hide');
     });
 
@@ -66,7 +64,7 @@ $(document).ready(function(){
     });
 
     // Collapse navbar
-    var navbarCollapse = function(){
+    var navbarCollapse = function () {
         if ($("#navigation").offset().top > 1000) {
             $("#navigation").removeClass("navbar-hover").addClass("navbar-shrink navbar-active");
             $(".navbar").css("opacity", 1);
@@ -77,5 +75,5 @@ $(document).ready(function(){
     navbarCollapse();
 
     // Collapse the navbar when page is scrolled
-    $(window).scroll(navbarCollapse);    
+    $(window).scroll(navbarCollapse);
 });
