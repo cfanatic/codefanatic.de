483df4735dd48b691218eed66e43a7039b6226e5
Arnd <git@codefanatic.de>
2020-03-29 07:33:06 +0200

    Add honeypot for email bot

--- js/contact.js
+++ js/contact.js
@@ -7,7 +7,6 @@ $(function () {
                 data: $(this).serialize(),
                 success: function (data) {
                     var messagebox = '<div class="alert ' + data.type + ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button><strong>' + data.message + '</strong></div>';
-
                     $("#contact-form").find(".form-success").html(messagebox);
                     $("#contact-form")[0].reset();
                 }
